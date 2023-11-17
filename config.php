84d98b1ef5a3252618854ccea10c13066caffff0
Joe Corporal <joelouie.corporal@my.utsa.edu>
2023-11-17 11:21:46 -0600

    testing db connection

--- config.php
+++ config.php
@@ -14,18 +14,7 @@
 
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $AZURE_SQL_DATABASE = getenv('AZURE_SQL_DATABASE');
-    $AZURE_SQL_PWD = getenv('AZURE_SQL_PWD');
-    $AZURE_SQL_SERVERNAME = getenv('AZURE_SQL_SERVERNAME');
-    $AZURE_SQL_UID = getenv('AZURE_SQL_UID');
-
-    echo "<h3> AZURE_SQL_DATABASE: $AZURE_SQL_DATABASE </br>
-    AZURE_SQL_PWD: $AZURE_SQL_PWD</br>
-    AZURE_SQL_SERVERNAME: $AZURE_SQL_SERVERNAME
-    </br> AZURE_SQL_UID: $AZURE_SQL_UID</br></h3>";
-
-
-    $conn = new PDO("sqlsrv:server = tcp:php-b2b-db-srvr.database.windows.net,1433; Database = $AZURE_SQL_DATABASE", $AZURE_SQL_UID, $AZURE_SQL_PWD);
+    $conn = new PDO("sqlsrv:server = tcp:$AZURE_SQL_SERVERNAME; Database = $AZURE_SQL_DATABASE", $AZURE_SQL_UID, $AZURE_SQL_PWD);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -35,7 +24,7 @@ catch (PDOException $e) {
 
 // SQL Server Extension Sample Code:
 $connectionInfo = array("UID" => $AZURE_SQL_UID, "pwd" => $AZURE_SQL_PWD, "Database" => $AZURE_SQL_DATABASE, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:php-b2b-db-srvr.database.windows.net,1433";
+$serverName = "tcp:$AZURE_SQL_SERVERNAME";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 ?>
