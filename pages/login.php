28ce3caa5e3bc430c50d3a019be9454d55b2f096
Joe Corporal <joelouie.corporal@my.utsa.edu>
2023-11-24 11:14:34 -0600

    debugging login.php connection with db

--- pages/login.php
+++ pages/login.php
@@ -68,7 +68,7 @@ session_start();
             if(empty($emailErr) && empty($passwordErr)){
               $tsql = "SELECT USER_FNAME, USER_LNAME, USER_ADMIN FROM [USER] 
                       WHERE ACTIVE = 1
-                      -- AND USER_PASSWORD = $userEmail AND USER_PASSWORD = $userPassword
+                      AND USER_PASSWORD = $userEmail AND USER_PASSWORD = $userPassword
                       ";
 
               $getUser = sqlsrv_query($conn, $tsql);
