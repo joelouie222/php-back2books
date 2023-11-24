a46b246cfe12071bfa258ee33e34e2a9b039e6f9
Joe Corporal <joelouie.corporal@my.utsa.edu>
2023-11-24 15:12:16 -0600

    fixing logout

--- logout.php
+++ logout.php
@@ -1,14 +1,10 @@
 <?php
-    include('/functions.php');
-    if (isset($_SESSION["loggedIn"]) AND $_SESSION["loggedIn"] == true){
-        $_SESSION["loggedIn"]="";
-        $_SESSION["fname"]="";
-        $_SESSION['lname']="";
-        $_SESSION["admin"]="";
-        $_SESSION["loginEmail"]="";
-        $_SESSION["hashedPassword"]="";
-        redirect("https://php-back2books.azurewebsites.net/");
-    } else {
-        redirect("https://php-back2books.azurewebsites.net/");
-    }
+    include('functions.php');
+    $_SESSION["loggedIn"]="";
+    $_SESSION["fname"]="";
+    $_SESSION['lname']="";
+    $_SESSION["admin"]="";
+    $_SESSION["loginEmail"]="";
+    $_SESSION["hashedPassword"]="";
+    redirect("https://php-back2books.azurewebsites.net/");
 ?>
