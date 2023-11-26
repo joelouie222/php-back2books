b89046e613419683d36671afc09a6f6e5a7c6d78
Joe Corporal <joelouie.corporal@my.utsa.edu>
2023-11-26 01:08:37 -0600

    included config.php

--- removeCartItem.php
+++ removeCartItem.php
@@ -1,13 +1,14 @@
 <?php
     session_start();
     include('functions.php');
+    include('config.php');
     if (isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"] == true) {
         if (isset($_GET['p']))
-        {
+        { 
             $itemId = $_GET['p'];
             $tsql = "DELETE FROM CART_ITEMS WHERE CITEM_ID = '$itemId'";
             $removeItem = sqlsrv_query($conn, $tsql);
-            if ($removeItem === false) {
+            if ($removeItem == false) {
                 die(print_r(sqlsrv_errors(), true));
             } else {
                 echo "Row deleted successfully.";
