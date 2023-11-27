e3c2f1b6b7754a01dedfacc072f42e174fa12764
nielmin <whadmia@gmail.com>
2023-11-27 17:49:03 -0600

    Fix typo in SQL query

--- pages/search.php
+++ pages/search.php
@@ -1,5 +1,5 @@
 <?php
-    include '../config.php'
+    include '/config.php'
 ?>
 <h1>Search page</h1>
 
@@ -7,7 +7,7 @@
 <?php
     if (isset($_POST['submit-search'])) {
         $search = mysqli_real_escape_string($conn, $_POST['search']);
-        $sql = "SELECt * FROM BOOKS WHERE book_title LIKE '%search%'";
+        $sql = "SELECT * FROM BOOKS WHERE book_title LIKE '%search%'";
         $result = mysqli_query($conn, $sql);
         $queryResult = mysqli_num_rows($result);
 
