3bc0e43e5606ed07bb03038cca30bc7f89d33f27
Andrea Mendez <andreacelestem11@gmail.com>
2023-11-27 16:38:53 -0600

    deleted info.php

--- config.php
+++ config.php
@@ -1,17 +1,4 @@
 <?php
-// Database configuration
-// define('DB_HOST', 'back2books.xyz');
-// define('DB_USER', 'admin');
-// define('DB_PASS', 'back2books');
-// define('DB_NAME', 'back2books');
-
-// $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-// // Check the database connection
-// if ($conn->connect_error) {
-//     echo("Connection failed: " . $conn->connect_error);
-// }
-
 
 // PHP Data Objects(PDO) Sample Code:
 // try {
@@ -37,12 +24,8 @@ $AZURE_SQL_UID = getenv('AZURE_SQL_UID');
 
 $connectionInfo = array("UID" => $AZURE_SQL_UID, "pwd" => $AZURE_SQL_PWD, "Database" => $AZURE_SQL_DATABASE, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:" . $AZURE_SQL_SERVERNAME;
-//$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 $connectionOptions = array("Database"=>$AZURE_SQL_DATABASE,"Uid"=>$AZURE_SQL_UID, "PWD"=>$AZURE_SQL_PWD);
-echo "<p>Server: $serverName</p><br>";
-echo "<p>Database: " . $connectionOptions["Database"] . "</p><br>";
-echo "<p>Username: " . $connectionOptions["Uid"] . "</p><br>";
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 if (!$conn) {
     die("Connection failed: " . print_r(sqlsrv_errors(), true));
