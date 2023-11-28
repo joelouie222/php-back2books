68cd15568224907457ae7d650ca0d39e66bb3460
nielmin <whadmia@gmail.com>
2023-11-27 19:13:27 -0600

    Fix SQL query for search

--- pages/search.php
+++ pages/search.php
@@ -7,11 +7,21 @@
 <?php
     if (isset($_POST['search'])) {
         $search = $_POST['search'];
-        $sql = "SELECT * FROM BOOKS WHERE 
-            BOOK_TITLE LIKE '%$search%' OR 
-            author_fname LIKE '%$search%' OR 
-            author_lname LIKE '%$search%'";
-        $result = sqlsrv_query($conn, $sql);
+        $tsql = " SELECT * FROM BOOKS B
+        INNER JOIN BOOK_IMAGE BI ON B.BOOK_ID = BI.BOOK_ID
+        INNER JOIN AUTHOR_LIST AL ON B.BOOK_ID = AL.BOOK_ID
+        INNER JOIN AUTHOR A ON AL.AUTHOR_ID = A.AUTHOR_ID
+        INNER JOIN PRODUCT_INVENTORY PI ON PI.BOOK_ID = B.BOOK_ID
+        WHERE BOOK_TITLE LIKE '%$search%' OR
+        BOOK_TITLE LIKE '%$search%' OR
+        author_fname LIKE '%$search%' OR 
+        author_lname LIKE '%$search%'";
+
+        // $sql = "SELECT * FROM BOOKS WHERE 
+        //     BOOK_TITLE LIKE '%$search%' OR 
+        //     author_fname LIKE '%$search%' OR 
+        //     author_lname LIKE '%$search%'";
+        $result = sqlsrv_query($conn, $tsql);
 
         if ($result == false) {
             die(print_r(sqlsrv_errors(), true));
