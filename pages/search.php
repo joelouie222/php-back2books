eda2a0e7404b1559758987f50f62cc9236732d0a
nielmin <whadmia@gmail.com>
2023-11-27 21:28:17 -0600

    Fix HTML formatting

--- pages/search.php
+++ pages/search.php
@@ -35,8 +35,7 @@
         while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
             echo "<a href='product.php?isbn=".$row['BOOK_ISBN']."'><div class='book-container'>
                 <h1>".$row['BOOK_TITLE']."</h1>
-                <p>".$row['author_fname'],$row['author_lname']."</p>
-                <p>".$row['author_lname']."</p>
+                <p>".$row['author_fname']." ".$row['author_lname']."</p>
                 <p>".$row['BOOK_ISBN']."</p>
                 <p>".$row['PRICE']."</p>
                 <p>".$row['INV_QUANTITY']."</p>
