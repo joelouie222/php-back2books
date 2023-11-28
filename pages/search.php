c36f8c862bd600f66a5e5cf78a69f16e9988eeb6
Andrea Mendez <andreacelestem11@gmail.com>
2023-11-28 11:03:00 -0600

    enhanced style of search results pg and product pg

--- pages/search.php
+++ pages/search.php
@@ -3,26 +3,27 @@
 include '../layout.php'
 ?>
 <html lang="us">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
 
-        <!-- FONT AWESOME -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
 
-        <!-- OUR CSS -->
-        <link rel="stylesheet" href="../style.css">
-        <link rel="stylesheet" href="../logo-style.css">
-        <link rel="icon" type="image/x-icon" href="/images/favicon/favicon-16x16.png">
-    </head>
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- OUR CSS -->
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../logo-style.css">
+    <link rel="icon" type="image/x-icon" href="/images/favicon/favicon-16x16.png">
+</head>
 
 <h1>Search page</h1>
 <center>
-<div class="search-container">
-<?php
+    <div class="window search-container">
+        <?php
 if (isset($_POST['search'])) {
     $search = $_POST['search'];
     $sortBy = $_POST['sortBy'];
@@ -56,17 +57,17 @@ if (isset($_POST['search'])) {
             <div class='book-info-container'>
             <h1>".$row['BOOK_TITLE']."</h1>
             <p>".$row['author_fname']." ".$row['author_lname']."</p>
-            <p>".$row['BOOK_ISBN']."</p>
-            <p>".$row['PRICE']."</p>
-            <p>".$row['INV_QUANTITY']."</p>
+            <p>ISBN: ".$row['BOOK_ISBN']."</p>
+            <p>Price: $".$row['PRICE']."</p>
+            <p>In stock: ".$row['INV_QUANTITY']."</p>
             </div>
+            </br>
+            <button class='search-add' name='submit' type='submit'> ADD TO CART </button>
             </div></a>";
-        echo '<div>
-            <button class="search-add" name="submit" type="submit"> ADD TO CART </button>
-            </div>';
     } 
 }
 ?>
-</div>
+    </div>
 </center>
+
 </html>
