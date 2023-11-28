92711a68eb68d89602e250c890e937a76aab66d9
nielmin <whadmia@gmail.com>
2023-11-27 20:27:52 -0600

    Add click-on-search-result goes to product page

--- pages/product.php
+++ pages/product.php
@@ -1,156 +1,32 @@
 <?php
-  session_start();
+include '../config.php';
 ?>
-<!DOCTYPE html>
-<html>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Back2Books</title>
+<h1>Product page</h1>
 
-    <!-- GOOGLE FONT -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
-
-
-    <!-- FONT AWESOME -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <!-- OUR CSS -->
-    <link rel="stylesheet" href="/style.css">
-    <link rel="icon" type="image/x-icon" href="../images/favicon/favicon-16x16.png">
-</head>
-
-<body id="home">
-    <?php include('../layout.php');
-    ?>  
-      
-    <div class="container">
-
-        </nav>
-        <section id="products" class="products">
-            <table width="100%">
-                <tr>
-                    <td colspan="2">
-                        <h2>Products</h2>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <img src="https://png.pngtree.com/png-clipart/20190904/original/pngtree-retro-book-free-material-png-image_4481187.jpg"
-                            alt="Product 1" alt="Temporary Product 1" width="100" height="100">
-                        <h3>Product 1</h3>
-                        <p>This is a product description.
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde magni repellendus nemo? Sit
-                            rerum unde rem
-                            enim commodi repellat laboriosam, dolore delectus velit molestiae, pariatur eius
-                            necessitatibus, officiis
-                            excepturi ab?
-                        </p>
-                        <button>Add to Cart</button>
-                    </td>
-
-                    <td>
-                        <img src="https://png.pngtree.com/png-clipart/20190904/original/pngtree-retro-book-free-material-png-image_4481187.jpg"
-                            alt="Product 2" alt="Temporary Product 2" width="100" height="100">
-                        <h3>Product 2</h3>
-                        <p>This is another product description. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Deserunt
-                            modi, accusantium perferendis vitae eos fugiat expedita repudiandae impedit omnis! Fugit
-                            ipsam amet
-                            debitis accusantium a atque nostrum, deleniti exercitationem odit?</p>
-                        <button>Add to Cart</button>
-                    </td>
-
-                </tr>
-
-                <tr>
-                    <td>
-                        <img src="https://png.pngtree.com/png-clipart/20190904/original/pngtree-retro-book-free-material-png-image_4481187.jpg"
-                            alt="Product 3" alt="Temporary Product 3" width="100" height="100">
-                        <h3>Product 3</h3>
-                        <p>This is a product description.
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde magni repellendus nemo? Sit
-                            rerum unde rem
-                            enim commodi repellat laboriosam, dolore delectus velit molestiae, pariatur eius
-                            necessitatibus, officiis
-                            excepturi ab?
-                        </p>
-                        <button>Add to Cart</button>
-                    </td>
-
-                    <td>
-                        <img src="https://png.pngtree.com/png-clipart/20190904/original/pngtree-retro-book-free-material-png-image_4481187.jpg"
-                            alt="Product 4" alt="Temporary Product 4" width="100" height="100">
-                        <h3>Product 4</h3>
-                        <p>This is another product description. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Deserunt
-                            modi, accusantium perferendis vitae eos fugiat expedita repudiandae impedit omnis! Fugit
-                            ipsam amet
-                            debitis accusantium a atque nostrum, deleniti exercitationem odit?</p>
-                        <button>Add to Cart</button>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>
-                        <img src="https://png.pngtree.com/png-clipart/20190904/original/pngtree-retro-book-free-material-png-image_4481187.jpg"
-                            alt="Product 5" alt="Temporary Product 5" width="100" height="100">
-                        <h3>Product 5</h3>
-                        <p>This is a product description.
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde magni repellendus nemo? Sit
-                            rerum unde rem
-                            enim commodi repellat laboriosam, dolore delectus velit molestiae, pariatur eius
-                            necessitatibus, officiis
-                            excepturi ab?
-                        </p>
-                        <button>Add to Cart</button>
-                    </td>
-
-                    <td>
-                        <img src="https://png.pngtree.com/png-clipart/20190904/original/pngtree-retro-book-free-material-png-image_4481187.jpg"
-                            alt="Product 6" alt="Temporary Product 6" width="100" height="100">
-                        <h3>Product 6</h3>
-                        <p>This is another product description. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Deserunt
-                            modi, accusantium perferendis vitae eos fugiat expedita repudiandae impedit omnis! Fugit
-                            ipsam amet
-                            debitis accusantium a atque nostrum, deleniti exercitationem odit?</p>
-                        <button>Add to Cart</button>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>
-                        <img src="https://png.pngtree.com/png-clipart/20190904/original/pngtree-retro-book-free-material-png-image_4481187.jpg"
-                            alt="Product 7" alt="Temporary Product 7" width="100" height="100">
-                        <h3>Product 7</h3>
-                        <p>This is a product description.
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde magni repellendus nemo? Sit
-                            rerum unde rem
-                            enim commodi repellat laboriosam, dolore delectus velit molestiae, pariatur eius
-                            necessitatibus, officiis
-                            excepturi ab?
-                        </p>
-                        <button>Add to Cart</button>
-                    </td>
-
-                    <td>
-                        <img src="https://png.pngtree.com/png-clipart/20190904/original/pngtree-retro-book-free-material-png-image_4481187.jpg"
-                            alt="Product 8" alt="Temporary Product 8" width="100" height="100">
-                        <h3>Product 8</h3>
-                        <p>This is another product description. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Deserunt
-                            modi, accusantium perferendis vitae eos fugiat expedita repudiandae impedit omnis! Fugit
-                            ipsam amet
-                            debitis accusantium a atque nostrum, deleniti exercitationem odit?</p>
-                        <button>Add to Cart</button>
-                    </td>
-                </tr>
-            </table>
-        </section>
-</body>
-
-</html>
+<div class="catalog-container">
+<?php
+    $isbn = $_GET['isbn'];
+    $tsql = "SELECT * FROM BOOKS B
+        INNER JOIN BOOK_IMAGE BI ON B.BOOK_ID = BI.BOOK_ID
+        INNER JOIN AUTHOR_LIST AL ON B.BOOK_ID = AL.BOOK_ID
+        INNER JOIN AUTHOR A ON AL.AUTHOR_ID = A.AUTHOR_ID
+        INNER JOIN PRODUCT_INVENTORY PI ON PI.BOOK_ID = B.BOOK_ID
+        BOOK_ISBN LIKE '%$isbn%'";
+    
+    $result = sqlsrv_query($conn, $tsql);
+    
+    if ($result == false) {
+        die(print_r(sqlsrv_errors(), true));
+    }
+    while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
+        echo "<a href='product.php?ISBN=".$row['BOOK_ISBN']."'><div class='book-container'>
+            <h1>".$row['BOOK_TITLE']."</h1>
+            <p>".$row['author_fname']."</p>
+            <p>".$row['author_lname']."</p>
+            <p>".$row['BOOK_ISBN']."</p>
+            <p>".$row['PRICE']."</p>
+            </div>";
+    } 
+?>
+</div>
