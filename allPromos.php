fe1a6e64ee3ebd7d1caae57a81b95963179e29a4
Joe Corporal <joelouie.corporal@my.utsa.edu>
2023-11-28 09:21:33 -0600

    finalizing add coupon

--- allPromos.php
+++ allPromos.php
@@ -43,61 +43,27 @@
 
                         if (isset($_POST["addPromo"]) && $_POST["addPromo"] == "go") {
                             $discname = $_POST['discname'];
-                            $discdisc = $_POST['discdisc'];
+                            $discdesc = $_POST['discdesc'];
                             $disctag = $_POST['disctag'];
                             $disccode = $_POST['disccode'];
-                            $discval = ($disctag / 10);
+                            $discval = ((100 - $disctag)/ 100);
   
-
                             echo 'discname: '.$discname.'';
-                            echo 'discdisc: '.$discdisc.'';
+                            echo 'discdesc: '.$discdesc.'';
                             echo 'disctag: '.$disctag.'';
                             echo 'disccode: '.$disccode.'';
                             echo 'discval: '.$discval.'';
 
+                            $tsql = "INSERT INTO DISCOUNT (DISCOUNT_CODE, DISCOUNT_VALUE, DISCOUNT_NAME, DISCOUNT_DESC, DISCOUNT_TAG)
+                            VALUES ('$disccode', '$discval', '$discname', '$discdesc', '$disctag')";
+
+                            $addCoupon = sqlsrv_query($conn, $tsql);
+
+                            if ($addCoupon == NULL){
+                                die(print_r(sqlsrv_errors(), true));  // Print detailed error information
+                            }
+                            redirect("https://php-back2books.azurewebsites.net/allPromos.php")
                         } 
-
-
-
-
-
-                                        // if (isset($_POST["userUpdate"]) && $_POST["userUpdate"] == "go") {
-                                        //     $userid = $_POST['userid'];
-                                        //     $useremail =$_POST['useremail'];
-                                        //     $userpass = md5($_POST['userpass']);
-                                        //     $userfname =$_POST['fname'];
-                                        //     $userlname = $_POST['lname'];
-                                        //     $useractive = $_POST['useractive'];
-                                        //     $usersq = $_POST['usersq'];
-                                        //     $usersa = $_POST['usersa'];
-
-                                        //     // echo 'userid: '.$userid.'';
-                                        //     // echo 'useremail: '.$useremail.'';
-                                        //     // echo 'userpass: '.$userpass.'';
-                                        //     // echo 'userfname: '.$userfname.'';
-                                        //     // echo 'userlname: '.$userlname.'';
-                                        //     // echo 'useractive: '.$useractive.'';
-                                        //     // echo 'usersq: '.$usersq.'';
-                                        //     // echo 'usersa: '.$usersa.'';
-
-                                        //     $tsql = "UPDATE B2BUSER
-                                        //     SET
-                                        //     USER_EMAIL = '$useremail', 
-                                        //     USER_PASSWORD = '$userpass',
-                                        //     USER_FNAME = '$userfname',
-                                        //     USER_LNAME = '$userlname',
-                                        //     USER_ACTIVE = '$useractive',
-                                        //     USER_SQ = '$usersq',
-                                        //     USER_SA = '$usersa'
-                                        //     WHERE USER_ID = '$userid'";
-
-                                        //     $updateUser = sqlsrv_query($conn, $tsql);
-                        
-                                        //     if ($updateUser === false) {
-                                        //         die(print_r(sqlsrv_errors(), true));  // Print detailed error information
-                                        //     }
-                                        //     redirect("https://php-back2books.azurewebsites.net/allUsers.php");
-                                        // }
 
                         if (isset($_GET['action']) && $_GET['action'] == "add") {
 
@@ -110,8 +76,8 @@
                             echo '  </div>';
 
                             echo '  <div class="form-group">';
-                            echo '    <label for="discdisc">Description: </label>';
-                            echo '    <textarea required name="discdisc"></textarea>';
+                            echo '    <label for="discdesc">Description: </label>';
+                            echo '    <textarea required name="discdesc" style="height: 228px; width: 421px;"></textarea>';
                             echo '  </div>';
 
                             echo '  <div class="form-group">';
