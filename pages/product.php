c09710c4b21eda9899b0e2ae680b8e277126d975
nielmin <whadmia@gmail.com>
2023-11-27 20:33:15 -0600

    Potential fix for product.php

--- pages/product.php
+++ pages/product.php
@@ -20,7 +20,7 @@ include '../config.php';
         die(print_r(sqlsrv_errors(), true));
     }
     while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
-        echo "<a href='product.php?ISBN=".$row['BOOK_ISBN']."'><div class='book-container'>
+        echo "<div class='book-container'>
             <h1>".$row['BOOK_TITLE']."</h1>
             <p>".$row['author_fname']."</p>
             <p>".$row['author_lname']."</p>
