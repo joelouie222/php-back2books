61372c5d9a14be15c65ef200443a8508cd1917c9
nielmin <whadmia@gmail.com>
2023-11-27 20:07:52 -0600

    Initial commit for sortBy Price

--- pages/search.php
+++ pages/search.php
@@ -3,11 +3,13 @@
 ?>
 <h1>Search page</h1>
 
-<div class="catalog-container">
+<div class="search-container">
 <?php
     if (isset($_POST['search'])) {
         $search = $_POST['search'];
-        $tsql = "SELECT * FROM BOOKS B
+        $sortBy = $_POST["sortBy"];
+
+        $sql = "SELECT * FROM BOOKS B
         INNER JOIN BOOK_IMAGE BI ON B.BOOK_ID = BI.BOOK_ID
         INNER JOIN AUTHOR_LIST AL ON B.BOOK_ID = AL.BOOK_ID
         INNER JOIN AUTHOR A ON AL.AUTHOR_ID = A.AUTHOR_ID
@@ -17,21 +19,32 @@
         author_fname LIKE '%$search%' OR 
         author_lname LIKE '%$search%'";
 
-        // $sql = "SELECT * FROM BOOKS WHERE 
-        //     BOOK_TITLE LIKE '%$search%' OR 
-        //     author_fname LIKE '%$search%' OR 
-        //     author_lname LIKE '%$search%'";
-        $result = sqlsrv_query($conn, $tsql);
-
-        if ($result == false) {
-            die(print_r(sqlsrv_errors(), true));
+        if ($sortBy == "priceLowToHigh") {
+            $sql .= " ORDER BY PRICE ASC";
+        } elseif ($sortBy == "priceHighToLow") {
+            $sql .= " ORDER BY PRICE DESC";
         }
-        while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
-            echo "<div class='book-container'>
+
+        // Prepare and execute the query
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(':search', '%' . $search . '%', PDO::PARAM_STR);
+        $stmt->execute();
+
+        // $result = sqlsrv_query($conn, $sql);
+
+        // if ($result == false) {
+        //     die(print_r(sqlsrv_errors(), true));
+        // }
+        
+        // Display the search results
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            echo "<div class='result-container'>
                 <h1>".$row['BOOK_TITLE']."</h1>
                 <p>".$row['author_fname']."</p>
                 <p>".$row['author_lname']."</p>
                 <p>".$row['BOOK_ISBN']."</p>
+                <p>".$row['BOOK_ISBN']."</p>
+                <p>".$row['PRICE']."</p>
                 </div>";
         } 
     }
