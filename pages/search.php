b7fa8cd639b61766208da4426e4cd93f60ff4c63
nielmin <whadmia@gmail.com>
2023-11-27 18:30:54 -0600

    Fix typo in search.php

--- pages/search.php
+++ pages/search.php
@@ -6,17 +6,15 @@
 <div class="catalog-container">
 <?php
     if (isset($_POST['search'])) {
-        // $search = mysqli_real_escape_string($conn, $_POST['search']);
-        $sql = "SELECT * FROM BOOKS WHERE BOOK_TITLE LIKE '%search%'";
+        $search = $_POST['search'];
+        $sql = "SELECT * FROM BOOKS WHERE BOOK_TITLE LIKE '%$search%'";
         $result = sqlsrv_query($conn, $sql);
 
         if ($result == false) {
             die(print_r(sqlsrv_errors(), true));
         }
         while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
-            echo "
-                <h3>".$row['BOOK_TITLE']."</h3>
-                ";
+            echo "<p>{$row['BOOK_TITLE']}</p>";
         } 
     }
 ?>
