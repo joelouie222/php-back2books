618aec51c48f09ecbd8643d272fb7d91b04f1030
nielmin <whadmia@gmail.com>
2023-11-27 20:09:45 -0600

    Revert "Initial commit for sortBy Price"
    This reverts commit 61372c5d9a14be15c65ef200443a8508cd1917c9.

--- pages/search.php
+++ pages/search.php
@@ -3,13 +3,11 @@
 ?>
 <h1>Search page</h1>
 
-<div class="search-container">
+<div class="catalog-container">
 <?php
     if (isset($_POST['search'])) {
         $search = $_POST['search'];
-        $sortBy = $_POST["sortBy"];
-
-        $sql = "SELECT * FROM BOOKS B
+        $tsql = "SELECT * FROM BOOKS B
         INNER JOIN BOOK_IMAGE BI ON B.BOOK_ID = BI.BOOK_ID
         INNER JOIN AUTHOR_LIST AL ON B.BOOK_ID = AL.BOOK_ID
         INNER JOIN AUTHOR A ON AL.AUTHOR_ID = A.AUTHOR_ID
@@ -19,32 +17,21 @@
         author_fname LIKE '%$search%' OR 
         author_lname LIKE '%$search%'";
 
-        if ($sortBy == "priceLowToHigh") {
-            $sql .= " ORDER BY PRICE ASC";
-        } elseif ($sortBy == "priceHighToLow") {
-            $sql .= " ORDER BY PRICE DESC";
+        // $sql = "SELECT * FROM BOOKS WHERE 
+        //     BOOK_TITLE LIKE '%$search%' OR 
+        //     author_fname LIKE '%$search%' OR 
+        //     author_lname LIKE '%$search%'";
+        $result = sqlsrv_query($conn, $tsql);
+
+        if ($result == false) {
+            die(print_r(sqlsrv_errors(), true));
         }
-
-        // Prepare and execute the query
-        $stmt = $conn->prepare($sql);
-        $stmt->bindValue(':search', '%' . $search . '%', PDO::PARAM_STR);
-        $stmt->execute();
-
-        // $result = sqlsrv_query($conn, $sql);
-
-        // if ($result == false) {
-        //     die(print_r(sqlsrv_errors(), true));
-        // }
-        
-        // Display the search results
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo "<div class='result-container'>
+        while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
+            echo "<div class='book-container'>
                 <h1>".$row['BOOK_TITLE']."</h1>
                 <p>".$row['author_fname']."</p>
                 <p>".$row['author_lname']."</p>
                 <p>".$row['BOOK_ISBN']."</p>
-                <p>".$row['BOOK_ISBN']."</p>
-                <p>".$row['PRICE']."</p>
                 </div>";
         } 
     }
