9a8e68a911d2b5441fb602484830c54a2e9a9a1b
nielmin <whadmia@gmail.com>
2023-11-27 19:21:19 -0600

    Fix HTML in search

--- pages/search.php
+++ pages/search.php
@@ -7,7 +7,7 @@
 <?php
     if (isset($_POST['search'])) {
         $search = $_POST['search'];
-        $tsql = " SELECT * FROM BOOKS B
+        $tsql = "SELECT * FROM BOOKS B
         INNER JOIN BOOK_IMAGE BI ON B.BOOK_ID = BI.BOOK_ID
         INNER JOIN AUTHOR_LIST AL ON B.BOOK_ID = AL.BOOK_ID
         INNER JOIN AUTHOR A ON AL.AUTHOR_ID = A.AUTHOR_ID
@@ -27,7 +27,7 @@
             die(print_r(sqlsrv_errors(), true));
         }
         while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
-            echo "div class='book-box'
+            echo "div class='book-container'>
                 <h1>".$row['BOOK_TITLE']."</h1>
                 <p>".$row['author_fname']."</p>
                 <p>".$row['author_lname']."</p>
